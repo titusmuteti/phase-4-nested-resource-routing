1ecb7257dc92a008bf5fd2a5ef33ff0a5715c952
Titus Mumo <titusmumo@gmail.com>
2022-11-29 20:30:20 +0300

    Done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -12,4 +12,14 @@ class DogHousesController < ApplicationController
     render json: { error: "Dog house not found" }, status: :not_found
   end
 
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
 end
